dc2d16701b069ac0c29c1e9579eea95a72af035e
saart <saar.turjeman79@spsmail.cuny.edu>
2023-04-12 20:25:32 -0400

    #9

--- WIKI.py
+++ WIKI.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-
+# URL of page
 url = 'https://en.wikipedia.org/wiki/List_of_Super_Bowl_champions'
 
 response = requests.get(url)
@@ -11,12 +11,10 @@ soup = BeautifulSoup(html_content, 'html.parser')
 
 table = soup.find('table', {'class': 'wikitable'})
 
-
 for row in table.find_all('tr'):
     cells = row.find_all('td')
     if cells:
         cell1_data = cells[0].get_text()
         cell2_data = cells[1].get_text()
-        print('Cell 1: ', cell1_data)
-        print('Cell 2: ', cell2_data)
-
+        print('Cell 1:', cell1_data)
+        print('Cell 2:', cell2_data)
